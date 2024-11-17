4f3d80ff159b3c987258163c4773331531f3d612
Ivan Kosovan <ivan.kosovan@sapient.pro>
2024-11-17 20:01:31 +0200

    Sending message to user by bot

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -1,12 +1,30 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Log;
+use Longman\TelegramBot\Exception\TelegramException;
+use Longman\TelegramBot\Request as TelegramBotRequest;
+use Longman\TelegramBot\Telegram;
 
 class TelegramController extends Controller
 {
     public function handleWebhook(Request $request): void
     {
-
+        Log::channel('telegram')->debug(json_encode($request->all()));
+        $botApiKey = config('telegram.bot_api_token');
+        $botUserName = config('telegram.bot_username');
+        try {
+            $telegram = new Telegram($botApiKey, $botUserName);
+            $telegram->handle();
+            TelegramBotRequest::sendMessage([
+                'chat_id' => 1246832006,
+                'text' => 'Hello, World!'
+            ]);
+        } catch (TelegramException $e) {
+            Log::channel('telegram')->error($e->getMessage());
+        }
     }
 }
