9d141870d910e3b69d27644fd63047bf4190777e
Ivan Kosovan <ivan.kosovan@sapient.pro>
2024-11-21 23:04:46 +0200

    Prepared User dto and add fields email and workspace_id

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Log;
+use App\Models\Mongo\User as MongoUser;
 use Longman\TelegramBot\Exception\TelegramException;
 use Longman\TelegramBot\Request as TelegramBotRequest;
 use Longman\TelegramBot\Telegram;
@@ -13,15 +14,27 @@ class TelegramController extends Controller
 {
     public function handleWebhook(Request $request): void
     {
-        Log::channel('telegram')->debug(json_encode($request->all()));
         $botApiKey = config('telegram.bot_api_token');
         $botUserName = config('telegram.bot_username');
+
         try {
             $telegram = new Telegram($botApiKey, $botUserName);
             $telegram->handle();
+
+            $user = MongoUser::firstOrCreate(
+                ['chat_id' => $request->all()['message']['chat']['id']],
+                [
+                    'username' => $request->all()['message']['from']['username'],
+                    'first_name' => $request->all()['message']['from']['first_name'],
+                    'last_name' => $request->all()['message']['from']['last_name'],
+                    'chat_id' => $request->all()['message']['chat']['id'],
+                    'language_code' => $request->all()['message']['from']['language_code']
+                ]
+            );
+
             TelegramBotRequest::sendMessage([
-                'chat_id' => $request->all()['message']['chat']['id'],
-                'text' => 'Hello, World!'
+                'chat_id' => $user->getChatId(),
+                'text' => 'Hello, ' . $user->getUserName()
             ]);
         } catch (TelegramException $e) {
             Log::channel('telegram')->error($e->getMessage());
--- app/Models/Mongo/User.php
+++ app/Models/Mongo/User.php
@@ -3,6 +3,7 @@
 namespace App\Models\Mongo;
 
 use MongoDB\Laravel\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Casts\ObjectId;
 
 class User extends Model
 {
@@ -12,13 +13,85 @@ class User extends Model
 
     protected $fillable = [
         'username',
+        'workspace_id',
         'first_name',
         'last_name',
         'chat_id',
         'language_code',
+        'email',
         'created_at',
         'updated_at'
     ];
 
     public $timestamps = true;
+
+    public function getUserName(): string
+    {
+        return $this->getAttribute('username');
+    }
+
+    public function setUserName(string $value): void
+    {
+        $this->setAttribute('username', $value);
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->getAttribute('first_name');
+    }
+
+    public function setFirstName(string $value): void
+    {
+        $this->setAttribute('first_name', $value);
+    }
+
+    public function getLastName(): string
+    {
+        return $this->getAttribute('last_name');
+    }
+
+    public function setLastName(string $value): void
+    {
+        $this->setAttribute('lasts_name', $value);
+    }
+
+    public function getChatId(): int
+    {
+        return $this->getAttribute('chat_id');
+    }
+
+    public function setChatId(int $value): void
+    {
+        $this->setAttribute('chat_id', $value);
+    }
+
+    public function getLanguageCode(): string
+    {
+        return $this->getAttribute('language_code');
+    }
+
+    public function setLanguageCode(string $value): void
+    {
+        $this->setAttribute('language_code', $value);
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->getAttribute('email');
+    }
+
+    public function setEmail(string $value): void
+    {
+        $this->setAttribute('email', $value);
+    }
+
+    public function getWorkspaceId(): ?ObjectId
+    {
+        return $this->getAttribute('workspace_id');
+    }
+
+    public function setWorkspaceId(ObjectId $value): void
+    {
+        $this->setAttribute('workspace_id', $value);
+    }
 }
