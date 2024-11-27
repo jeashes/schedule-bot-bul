370e8c0d86c9ad0965fc88b39d07267475e0bc3f
Ivan Kosovan <ivan.kosovan@sapient.pro>
2024-11-27 22:50:37 +0200

    Prepared messages for bot on determine user studying data

--- resources/lang/en/bot_messages.php
+++ resources/lang/en/bot_messages.php
@@ -6,5 +6,15 @@ return [
                 "\nin convinient Trello system. We will create schedule plan on next" .
                 "\n2 weeks." .
                 "\nWe will take into account: *topic*, *time on studiyng* and *pace level.*" .
-                "\n*If you are really want to improve yourself, push on button*"
+                "\n*If you are really want to improve yourself, push on button*",
+
+    'subject_of_studies' => 'Please tell me the name of your study subject!',
+
+    'total_hours_on_study' => 'How many hours you give yourself?' .
+                            'Please think about it so much carefully as you can',
+
+    'pace_level' => 'What pace level you want take for education: *easy*, *medium*, *hard*',
+
+    'ask_email' => 'On the last give me your email adress. I will use for creating workspace for you!'
+
 ];
