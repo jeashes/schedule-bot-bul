f76c78c4737d268e08628c8729a0bb5c5b052fc6
Ivan Kosovan <ivan.kosovan@sapient.pro>
2024-12-30 23:15:21 +0200

    Prepared trello cards api manager

--- app/Interfaces/Trello/CardsApiInterface.php
+++ app/Interfaces/Trello/CardsApiInterface.php
@@ -9,40 +9,40 @@ interface CardsApiInterface
     // idMembers and idLabels shoud be commad separated list of member ids
     public function createNewCard(
         string $idList,
-        string $name,
-        string $desc,
-        string $position,
-        string $dueDate,
-        string $startDate,
-        bool $dueCompleteDate,
-        string $idMembers,
-        string $idLabels,
-        string $urlSource,
-        string $idCardSource
+        ?string $name,
+        ?string $desc,
+        ?string $position,
+        ?string $dueDate,
+        ?string $startDate,
+        ?bool $dueCompleteDate,
+        ?string $idMembers,
+        ?string $idLabels,
+        ?string $urlSource,
+        ?string $idCardSource
     ): Response;
 
     public function getCard(string $idCard): Response;
 
     public function updateCard(
         string $idCard,
-        string $name,
-        string $desc,
-        bool $closed,
-        string $idMembers,
-        string $idAttachmentCover,
-        string $idList,
-        string $idLabels,
-        string $idBoard,
-        string $position,
-        string $dueDate,
-        string $startDate,
-        bool $dueCompleteDate,
-        bool $subscribed
+        ?string $name,
+        ?string $desc,
+        ?bool $closed,
+        ?string $idMembers,
+        ?string $idAttachmentCover,
+        ?string $idList,
+        ?string $idLabels,
+        ?string $idBoard,
+        ?string $position,
+        ?string $dueDate,
+        ?string $startDate,
+        ?bool $dueCompleteDate,
+        ?bool $subscribed
     ): Response;
 
-    public function deleteCard(string $idCard, string $field): Response;
+    public function deleteCard(string $idCard): Response;
 
-    public function getBoardOfCard(string $idCard): Response;
+    public function getBoardOfCard(string $idCard, string $fields): Response;
 
     public function addNewCommentToCard(string $idCard, string $text): Response;
 
@@ -50,7 +50,7 @@ interface CardsApiInterface
 
     public function addMemberToCard(string $idCard, string $idMember): Response;
 
-    public function createNewLabel(string $idCard, string $color, string $name): Response;
+    public function createNewLabel(string $idCard, string $color, ?string $name): Response;
 
     public function markCardNotificationAsRead(string $idCard): Response;
 
