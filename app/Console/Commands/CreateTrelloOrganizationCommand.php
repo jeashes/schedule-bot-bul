d5224c7c56f258ec779a26a8df3ffe4f9594ae93
Ivan Kosovan <ivan.kosovan@sapient.pro>
2025-01-07 01:35:40 +0200

    Modified creating organization in trello to avoid duplication

--- app/Console/Commands/CreateTrelloOrganizationCommand.php
+++ app/Console/Commands/CreateTrelloOrganizationCommand.php
@@ -7,6 +7,7 @@ use App\Models\Mongo\Organization;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Predis\Command\Redis\DUMP;
 use Throwable;
 
 class CreateTrelloOrganizationCommand extends Command
@@ -30,47 +31,37 @@ class CreateTrelloOrganizationCommand extends Command
      */
     public function handle(OrganizationManager $manager)
     {
-        $displayName = 'Schedule Bot Bull';
-        $name = 'Main organization';
+        $displayName = 'Zabuliti velika';
+        $name = 'Main orga';
         $desription = 'The organization where creates boards for scheduling';
-        $organizationId = config('trello.organization_id');
-
-        if (!$this->isTrelloOrgAlreadyCreated($organizationId, $manager)) {
-            $organizationId = $manager->create(
-                displayName: $displayName,
-                name: $name,
-                description: $desription
-            );
+        if ($this->isTrelloOrgAlreadyCreated(config('trello.organization_id'), $manager)) {
+            $this->info('Organization id already exist in config/trello');
+            die();
         }
 
-        try {
-            Organization::query()->createOrFirst([
-                'display_name' => $displayName,
-                'name' => $name,
-                'description' => $desription,
-                'trello_id' => $organizationId
-            ]);
+        $response = $manager->create(
+            displayName: $displayName,
+            name: $name,
+            description: $desription
+        );
 
-            Log::channel('trello')->info('Main Trello Organization was successfuly created');
-        } catch (Throwable $exception) {
-            Log::channel('trello')->error('Something went wrong', [
-                'error'=> $exception->getMessage(),
-                'organizationTrelloId' => $organizationId
-            ]);
-        }
+        $data = json_decode($response->body(), true);
+        $organizationId = $data['id'];
+
+        $this->info("Your organization id successfully created: $organizationId, please add into config/trello");
     }
 
     private function isTrelloOrgAlreadyCreated(string $orgId, OrganizationManager $organizationManager): bool
     {
         try {
             $response = $organizationManager->get($orgId);
-            if (!$response->status() === 200) {
-                Log::channel('trello')->info('Needs created Trello Organization and add id from response to config');
-                return false;
+            if ($response->status() === 200) {
+                Log::channel('trello')->info('Organization already created');
+                return true;
             }
 
-            Log::channel('trello')->info('Organization already created');
-            return true;
+            Log::channel('trello')->info('Needs created Trello Organization and add id from response to config');
+            return false;
         } catch (Throwable $e) {
             Log::channel('trello')->info('Error during getting organization id from Trello API '
                 . $e->getMessage()
