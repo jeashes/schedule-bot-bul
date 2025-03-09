d6a81e3049f039eaa37d01b0da50a5a361d45bbb
Ivan Kosovan <ivan.kosovan@sapient.pro>
2025-03-10 01:17:53 +0200

    Saving goal, knowledge_level, course_type, tools in workspace model

--- app/Repository/TrelloWorkSpaceRepository.php
+++ app/Repository/TrelloWorkSpaceRepository.php
@@ -2,9 +2,13 @@
 
 namespace App\Repository;
 
+use App\Enums\Telegram\CourseTypeEnum;
+use App\Enums\Telegram\GoalEnum;
 use App\Enums\Telegram\HoursOnStudyEnum;
+use App\Enums\Telegram\KnowledgeLevelEnum;
 use App\Enums\Telegram\ScheduleEnum;
 use App\Enums\Telegram\SubjectStudiesEnum;
+use App\Enums\Telegram\ToolsEnum;
 use App\Models\Mongo\User as MongoUser;
 use App\Models\Mongo\Workspace;
 use Illuminate\Support\Facades\Redis;
@@ -24,11 +28,19 @@ class TrelloWorkSpaceRepository
         $subjectStudiesInfo = json_decode(Redis::get($userId.'_'.SubjectStudiesEnum::QUESTION->value), true);
         $hoursOnStudyInfo = json_decode(Redis::get($userId.'_'.HoursOnStudyEnum::QUESTION->value), true);
         $scheduleInfo = json_decode(Redis::get($userId.'_'.ScheduleEnum::QUESTION->value), true);
+        $goalInfo = json_decode(Redis::get($userId.'_'.GoalEnum::QUESTION->value), true);
+        $toolsInfo = json_decode(Redis::get($userId.'_'.ToolsEnum::QUESTION->value), true);
+        $knowledgeLevelInfo = json_decode(Redis::get($userId.'_'.KnowledgeLevelEnum::QUESTION->value), true);
+        $courseTypeInfo = json_decode(Redis::get($userId.'_'.CourseTypeEnum::QUESTION->value), true);
 
         return [
             'name' => $subjectStudiesInfo['current_answer'],
             'time_on_schedule' => (float) $hoursOnStudyInfo['current_answer'],
             'schedule' => $scheduleInfo['current_answer'],
+            'goal' => $goalInfo['current_answer'],
+            'tools' => $toolsInfo['current_answer'],
+            'knowledge_level' => $knowledgeLevelInfo['current_answer'],
+            'course_type' => $courseTypeInfo['current_answer'],
             'task_ids' => [],
         ];
     }
