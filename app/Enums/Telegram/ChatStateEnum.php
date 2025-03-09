74970d9bc50101bf4b2818238065b29e1e96de8f
Ivan Kosovan <ivan.kosovan@sapient.pro>
2025-03-09 18:30:31 +0200

    Added goal, knowledge_level, tools, course_type state to main enum

--- app/Enums/Telegram/ChatStateEnum.php
+++ app/Enums/Telegram/ChatStateEnum.php
@@ -6,9 +6,13 @@ enum ChatStateEnum: int
 {
     case START = 0;
     case SUBJECT_STUDY = 1;
-    case HOURS = 2;
-    case SCHEDULE = 3;
-    case EMAIL = 4;
-    case FINISHED = 5;
-    case USER_HAS_WORKSPACE = 6;
+    case GOAL = 2;
+    case KNOWLEDGE_LEVEL = 3;
+    case COURSE_TYPE = 4;
+    case TOOLS = 5;
+    case HOURS = 6;
+    case SCHEDULE = 7;
+    case EMAIL = 8;
+    case FINISHED = 9;
+    case USER_HAS_WORKSPACE = 10;
 }
