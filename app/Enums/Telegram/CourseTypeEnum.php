259152e3c8cc06ada10e3883ff67fcd8ac6a944c
Ivan Kosovan <ivan.kosovan@sapient.pro>
2025-03-09 17:23:42 +0200

    Prepared enums for redis key value: tools, goal, knowledge_level, course_type

---
+++ app/Enums/Telegram/CourseTypeEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums\Telegram;
+
+enum CourseTypeEnum: string
+{
+    case QUESTION = 'user_course_type_question';
+    case KEY_NAME = 'user_course_type';
+}
---
+++ app/Enums/Telegram/GoalEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums\Telegram;
+
+enum GoalEnum: string
+{
+    case QUESTION = 'user_goal_question';
+    case KEY_NAME = 'user_goal';
+}
